12ae9eba7152000624275ef4a7cd07386293b7b5
fenudev <fenudev@gmail.com>
2016-08-29 10:34:07 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,23 @@
+local alchemist 	= require("Fenudev.Gather.alc");
+local farmer 		= require("Fenudev.Gather.far");
+local fisherman		= require("Fenudev.Gather.fis");
+local lumberjack 	= require("Fenudev.Gather.lum");
+local miner 		= require("Fenudev.Gather.min");
+
+function move()
+	return{
+	
+	}
+end
+
+function phenix()
+	return{
+	
+	}
+end
+
+function bank()
+	return{
+
+	}
+end
