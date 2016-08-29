7629b4664f9c952b70989cac9de075c8ab50e90b
fenudev <fenudev@gmail.com>
2016-08-29 10:36:19 -0300

    Update directory of main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
-local alchemist 	= require("Fenudev.Gather.alc");
-local farmer 		= require("Fenudev.Gather.far");
-local fisherman		= require("Fenudev.Gather.fis");
-local lumberjack 	= require("Fenudev.Gather.lum");
-local miner 		= require("Fenudev.Gather.min");
+local alchemist 	= require("Dofus_Bot.Gather.alc");
+local farmer 		= require("Dofus_Bot.Gather.far");
+local fisherman		= require("Dofus_Bot.Gather.fis");
+local lumberjack 	= require("Dofus_Bot.Gather.lum");
+local miner 		= require("Dofus_Bot.Gather.min");
 
 function move()
 	return{
